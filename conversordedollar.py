868b7734f13cd774a19b4b4832fb20815d3fd861
Alexssilvadev <112443124+AlexSSilvaDev@users.noreply.github.com>
2022-11-30 10:59:55 -0300

    Add files via upload

---
+++ conversordedollar.py
@@ -0,0 +1,5 @@
+real = float(input('Quantos reais voce ira converter em dollar? '))
+dollar = real / 5.36
+print('com R$ {:.2f} você pode comprar US$ {:.2f} considerando 1 dollar a 5.36 reais'.format(real,dollar))
+
+
